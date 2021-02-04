6aba1a1e8a2c40329fe02249af392d355919014a
Efraim Gentil <efraim.gentil@gmail.com>
2021-02-05 00:30:37 +0100

    Working on integratio ntest for polling service

--- src/test/kotlin/me/efraimgentil/seeker/AbstractIT.kt
+++ src/test/kotlin/me/efraimgentil/seeker/AbstractIT.kt
@@ -2,10 +2,12 @@ package me.efraimgentil.seeker
 
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
         , properties = ["spring.profiles.active=local,test"])
+@AutoConfigureWireMock(port = 0)
 abstract class AbstractIT{
 }
--- src/test/kotlin/me/efraimgentil/seeker/service/PollingExpenseServiceIT.kt
+++ src/test/kotlin/me/efraimgentil/seeker/service/PollingExpenseServiceIT.kt
@@ -1,8 +1,17 @@
 package me.efraimgentil.seeker.service
 
+import com.github.tomakehurst.wiremock.client.WireMock
 import me.efraimgentil.seeker.AbstractIT
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
+import org.springframework.core.io.ClassPathResource
+import org.springframework.util.ResourceUtils
+import java.io.ByteArrayOutputStream
+import java.nio.file.Files
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+
 
 class PollingExpenseServiceIT : AbstractIT() {
 
@@ -10,6 +19,23 @@ class PollingExpenseServiceIT : AbstractIT() {
 
     @Test
     fun test(){
+        val file = ResourceUtils.getFile("json/expenses-example.json")
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        val zipOutputStream = ZipOutputStream(byteArrayOutputStream)
+        zipOutputStream.putNextEntry(ZipEntry("Ano-2018.json"))
+        zipOutputStream.write(Files.readAllBytes(file.absolutePath()))
+        zipOutputStream.closeEntry()
+        zipOutputStream.close()
+        WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/cotas/Ano-2018.json.zip"))
+                .willReturn(WireMock.aResponse()
+                        .withHeader("Content-Type" , "application/zip")
+                        .withBody(byteArrayOutputStream.toByteArray()))
+        )
+
+        pollingExpenseService.pullYear(2018)
+
+
+
 //        pollingExpenseService.downloadJsonZip(2018)
     }
 
